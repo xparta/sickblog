103fce53795400622dc2b0ef12c708a704c21cd9
xparta <tomarobaski@gmail.com>
2015-07-23 14:12:28 +0300

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,19 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+ i would very much love to work with you guys.I am a rails and ruby enthusiast with amazing social skills, i happened to graduate from Nairobi's only hackers school but unfortunately i never got the chance to intern  so i know i will be a very valuable asset to you guys if you consider me .I am open to learning new things and working with various people from different backgrounds towards a common goal and yes!i am a team player .looking forward to hearing from you guys .
+
+Thank you 
